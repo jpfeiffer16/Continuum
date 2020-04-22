520d3aec48758dc2855f7dfe8a3a35cc549672fe
jpfeiffer16 <joepfeiffer16@gmail.com>
2020-04-21 20:07:49 -0600

    Messing around with a few options

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 make:
 	mkdir -p ./bin
-	gcc ./continuum.c -o ./bin/cont -lX11 -lXext -g
+	gcc ./continuum.c -o ./bin/cont -lX11 -lXext -ggdb
--- continuum.c
+++ continuum.c
@@ -1,13 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <X11/Xlib.h>
 
-int main(argc, argv)
+#define BSIZE 1638
+#define ARGSIZE 256
+
+int main()
 {
-    Display *display;
-    display = XOpenDisplay(NULL);
-    int screen = XDefaultScreen(display);
-    Window root_window = XRootWindow(display, screen);
-    int width = DisplayWidth(display, screen);
-    int height = DisplayHeight(display, screen);
-    XWarpPointer(display, None, None, 0, 0, 0, 0, 10, 10);
-    XFlush(display);
+    //Open STDIN
+    
+    int count;
+    char buf[BSIZE];
+    while ((count = fread(buf, 1, BSIZE, stdin)) > 0)
+    {
+        /* fprintf(stdout, "%s", buf); */
+        /* fflush(stdout); */
+        char *args[ARGSIZE];
+        char *arg_ptr;
+        int arg_index = 0;
+        switch(buf[0])
+        {
+            case 'c':
+                system("xdotool click 1");
+                break;
+            case 'm':
+                while ((arg_ptr = strtok(buf, "|")) != NULL)
+                {
+                    args[arg_index] = arg_ptr;
+                    ++arg_index;
+                }
+                printf("Hello");
+                break;
+        }
+    }
+    
+    /* Display *display; */
+    /* display = XOpenDisplay(NULL); */
+    /* int screen = XDefaultScreen(display); */
+    /* Window root_window = XRootWindow(display, screen); */
+    /* int width = DisplayWidth(display, screen); */
+    /* int height = DisplayHeight(display, screen); */
+    /* XWarpPointer(display, None, None, 0, 0, 0, 0, 10, 10); */
+    /* XFlush(display); */
+    return 0;
 }
